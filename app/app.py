d11a98c5f7b44d35f1d411bf378285fc9eb9e1ca
basak-anushka13 <basakanu1313@gmail.com>
2025-07-27 01:02:17 +0530

    Final fracture detection app with YOLOv5 and .gitkeep added

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from pathlib import Path
 # Paths
 BASE_DIR = Path(__file__).resolve().parent
 YOLOV5_DIR = os.path.join(BASE_DIR, 'app', 'yolov5')  # yolov5 inside app/
-MODEL_PATH = BASE_DIR / 'app' / 'model' / 'best_windows.pt'  # model inside app/model/
+MODEL_PATH = BASE_DIR / 'model' / 'best_windows.pt'  # model inside app/model/
 
 # Add YOLOv5 to Python path
 sys.path.append(str(YOLOV5_DIR))
